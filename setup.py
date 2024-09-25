c59e862c423bb9fcd02fbe6389a1866dab515b4a
Hassan Mehmood <mehassanhmood@gmail.com>
2024-09-24 23:01:50 -0400

    updates pushed

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mod",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    description="Test Module",
+    author="Hassan Mehmood",
+    author_email="mehassanhmood@gmail.com",
+    url="https://github.com/mehassanhmood/py_portability.git"
+)
