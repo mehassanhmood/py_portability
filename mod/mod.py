cb5aa297770b10bdcd9d217be181f52cccc4da7f
Hassan Mehmood <mehassanhmood@gmail.com>
2024-09-24 23:04:57 -0400

    updated

--- mod/mod.py
+++ mod/mod.py
@@ -1,2 +1,12 @@
-def adding(x,y):
-    return x+y
+def addition(a, b):
+    """
+    This function takes two numbers and returns their sum.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The sum of the two numbers.
+    """
+    return a + b
